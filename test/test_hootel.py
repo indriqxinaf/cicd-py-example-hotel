7971f562d12ad4dacfae52ce3ab6ab15b6e93e71
indriqxinaf <142670228+indriqxinaf@users.noreply.github.com>
2024-01-10 19:34:06 +0100

    Update test_hootel.py

--- test/test_hootel.py
+++ test/test_hootel.py
@@ -27,6 +27,7 @@ class TestHootel(object):
     @allure.severity(allure.severity_level.TRIVIAL)
     @allure.tag("login")
     def test_login(self, email, password):
+        
         login_btn = WebDriverWait(self.browser, ).until(EC.element_to_be_clickable((By.XPATH, '//a[@class="nav-link"]')))
         login_btn.click()
 
