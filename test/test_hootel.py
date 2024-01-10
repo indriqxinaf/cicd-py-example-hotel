0b27e59338cdf426e57de9df47c2b4d40c75eed7
indriqxinaf <142670228+indriqxinaf@users.noreply.github.com>
2024-01-10 19:38:20 +0100

    Update test_hootel.py

--- test/test_hootel.py
+++ test/test_hootel.py
@@ -27,8 +27,9 @@ class TestHootel(object):
     @allure.severity(allure.severity_level.TRIVIAL)
     @allure.tag("login")
     def test_login(self, email, password):
-        
-        login_btn = WebDriverWait(self.browser, ).until(EC.element_to_be_clickable((By.XPATH, '//a[@class="nav-link"]')))
+        menu_toggle = WebDriverWait(self.browser, 5).until(EC.element_to_be_clickable((By.XPATH, '//button[@class="navbar-toggler collapsed"]')))
+        menu_toggle.click()
+        login_btn = WebDriverWait(self.browser, 5).until(EC.element_to_be_clickable((By.XPATH, '//a[@class="nav-link"]')))
         login_btn.click()
 
         email_input = self.browser.find_element(By.ID, 'email')
@@ -41,7 +42,7 @@ class TestHootel(object):
         submit_btn.click()
         time.sleep(1)
 
-        logout_btn = self.browser.find_element(By.ID, 'logout-link')
+        logout_btn = WebDriverWait(self.browser, 5).until(EC.element_to_be_clickable((By.ID, 'logout-link')))
 
         assert logout_btn.text == "Kilépés"
 
